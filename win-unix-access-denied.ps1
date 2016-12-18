c69d9d4e792fd9ea3482752cf06ff7c9ae7e6310
BokhanGV <bokhangv@gmail.com>
2016-12-18 13:34:16 +0300

    Update win-unix-access-denied.ps1

--- win-unix-access-denied.ps1
+++ win-unix-access-denied.ps1
@@ -10,9 +10,10 @@ If (!(Test-Path $workdir)){mkdir $workdir}
 ##### LOGIC
 
 # get list of files that face Permission Denied error and put the list into result file
-select-string -path $target "Can't open file:([/]\s*\w*)*" | select matches | Out-File $workdir\tempfile.txt
-select-string -path $workdir\tempfile.txt "[/]\s*\w*([/]\s*\w*)*" | select matches | Out-File $workdir\$result 
+select-string -path $target        "Can't open file:([/]\s*\w*)*" | select matches > $workdir\tempfile.txt
+select-string -path $workdir\tempfile.txt "[/]\s*\w*([/]\s*\w*)*" | select matches > $workdir\$result 
 
+# clear the mess
 Remove-Item $workdir\tempfile.txt
 
 # open the path in a default file browser
