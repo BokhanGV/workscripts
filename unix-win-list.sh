9657384277e2fcd5e293057abc4c5c1fd8794790
BokhanGV <bokhangv@gmail.com>
2016-12-24 05:33:01 +0300

    Update unix-win-list.sh

--- unix-win-list.sh
+++ unix-win-list.sh
@@ -4,7 +4,7 @@
 
 target=$1					# argument 1 is the log file to extract the list from
 result=`basename $0 | cut -f1 -d'.'`".txt"	# result file name is e.g. script-list.txt
-workdir=~/worktempdir				# where to put the list
+workdir=./worktempdir				# where to put the list
 if [ ! -d $workdir ]; then
 	mkdir $workdir				# if workdir does not exist create it
 fi
