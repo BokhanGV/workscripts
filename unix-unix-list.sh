f9268ea062782223d4a344aeea2bcdf655592ce5
BokhanGV <bokhangv@gmail.com>
2016-12-24 05:18:23 +0300

    Update unix-unix-list.sh

--- unix-unix-list.sh
+++ unix-unix-list.sh
@@ -3,16 +3,18 @@
 ##### PREREQUISITES
 
 target=$1							# argument 1 is the log file to extract the list from
-result=`basename $0 | cut -f1 -d'.'`"-list.txt"		# result file name is e.g. script-list.txt
+result=`basename $0 | cut -f1 -d'.'`".txt"			# result file name is e.g. script.txt
 workdir=./worktempdir						# where to put the list
-if [ ! -d $tempdir ]; then
-	mkdir $workdir						# if workdir does not exist create it
-fi
+if [ ! -d $workdir ];then mkdir $workdir;fi			# if workdir does not exist create it
 
 ##### LOGIC
 
+textPermissionDenied="Can't open file:"
+rePermissionDenied="[/](.*){,}"
+
 # get list of files that face Permission Denied error and put the list into result file
-grep "Can't open file:" $target | grep -Eo "[/][a-zA-Z0-9.,\ _-'\"\!@#$%^&*()\-+]{,}" | sort | uniq > $workdir/$result
+echo "Permission denied:" > $workdir/$result
+grep "$textPermissionDenied" $target | grep -Eo "$rePermissionDenied" | sort | uniq >> $workdir/$result
 
 ## TODO files not found
 
