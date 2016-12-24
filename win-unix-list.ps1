cd64b007694aa5f09767a2f99481624afbfc4aa4
BokhanGV <bokhangv@gmail.com>
2016-12-24 03:45:00 +0300

    Update win-unix-list.ps1

--- win-unix-list.ps1
+++ win-unix-list.ps1
@@ -13,7 +13,7 @@ $rePermissionDenied="Can't open file:(.*)$"
 
 # get list of files that face Permission Denied error and put the list into result file
 echo "Permission denied:" > $workdir\$result
-select-string -path $target $rePermissionDenied | Sort-Object | Get-Unique | ForEach-Object {$_.Matches.Groups[1].Value} > $workdir\tempresult.txt 
+select-string -path $target $rePermissionDenied   | ForEach-Object {$_.Matches.Groups[1].Value} > $workdir\tempresult.txt 
 Get-Content $workdir\tempresult.txt | Sort-Object | Get-Unique >> $workdir\$result
 
 ## TODO files not found
