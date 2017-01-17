682bf76ab5010b24c9e61e7da39c1fc2d9246c07
BokhanGV <bokhangv@gmail.com>
2017-01-17 05:52:28 +0300

    Update win-unix-list.ps1

--- win-unix-list.ps1
+++ win-unix-list.ps1
@@ -1,30 +1,37 @@
 ##### PREREQUISITES
 
 param (
-	[string]$target=$(throw "-target is required."),				# argument 1 is the log file to extract the list from
-	[string]$result=([io.fileinfo]$MyInvocation.MyCommand.Name).basename+".txt",	# result file name is e.g. script-list.txt
-    	[string]$workdir=".\worktempdir"						# where to put the list
+	[string]$target=$(throw "-target is required."),								# argument 1 is the log file to extract the list from
+    [string]$dbg=$(),
+	[string]$result=([io.fileinfo]$MyInvocation.MyCommand.Name).basename+".txt",	# result file name is e.g. script.txt
+    [string]$workdir="."															# where to put the list
 )
-If (!(Test-Path $workdir)){mkdir $workdir}                                         	# if workdir does not exist create it
+#if (!(Test-Path $workdir)){mkdir $workdir}											# if workdir does not exist create it
+
+##### GIVEN
+
+$rePermissionDenied="Can't open file:(.*)$"
+$reNotFound="/(.*) does not exist$"
 
 ##### LOGIC
 
-$rePermissionDenied="Can't open file:(.*)$"
-$reNotFound="[ERROR]: Error Search item  [/](.*){,} does not exist$"
+echo " `r`n Permission denied: `r`n" > $workdir\$result
+if ($dbg){invoke-item $workdir\$result; echo "Created and opened. Press any key to continue";		$pressed=$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")} #DBG
 
-# get list of files that face Permission Denied error and put the list into result file
+select-string -path $target $rePermissionDenied		| ForEach-Object {$_.Matches.Groups[1].Value} > $workdir\tempresult.txt   # for Permission Denied
+Get-Content $workdir\tempresult.txt | Sort-Object	| Get-Unique >> $workdir\$result
+if ($dbg){invoke-item $workdir\$result; echo "Permission denied done. Press any key to continue";	$pressed=$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")} #DBG
 
-echo "Permission denied:" > $workdir\$result
-select-string -path $target $rePermissionDenied   | ForEach-Object {$_.Matches.Groups[1].Value} > $workdir\tempresult.txt 
-Get-Content $workdir\tempresult.txt | Sort-Object | Get-Unique >> $workdir\$result
-
-echo "" >> $workdir\$result
-echo "Could not find a part of the path:" >> $workdir\$result
-select-string -path $target $rePathNotFound       | ForEach-Object {$_.Matches.Groups[1].Value} > $workdir\tempresult.txt   # for not found files
-Get-Content $workdir\tempresult.txt | Sort-Object | Get-Unique >> $workdir\$result
+echo " `r`n Could not find a part of the path: `r`n" >> $workdir\$result
+select-string -path $target $reNotFound				| ForEach-Object {$_.Matches.Groups[1].Value} > $workdir\tempresult.txt   # for not found files
+Get-Content $workdir\tempresult.txt | Sort-Object	| Get-Unique >> $workdir\$result
+if ($dbg){invoke-item $workdir\$result; echo "Not found done. Press any key to continue";			$pressed=$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")} #DBG
 
 # clean the mess up
 rm $workdir\tempresult.txt
 
+##### OUTPUT
+
 # open the path in a default file browser
-invoke-item $workdir #\$result
+#invoke-item $workdir			# for sending
+invoke-item $workdir\$result	# for reading
